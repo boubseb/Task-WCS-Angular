cb9c2d2784cd8401c1463a7410d3575cd2a09e19
Sebastien Peltier <sebpel62@gmail.com>
2024-05-27 21:57:26 +0200

    update

---
+++ src/app/task-pokedex/pages/pokedex-page/pokedex-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokedex-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './pokedex-page.component.html',
+  styleUrl: './pokedex-page.component.scss'
+})
+export class PokedexPageComponent {
+
+}
